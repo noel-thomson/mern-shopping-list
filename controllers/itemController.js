b83ba9b8fec2ec06712eea8429f27e641bae7a0a
Noel Thomson <noel.thomson@gmail.com>
2020-12-29 14:26:12 -0500

    backend working

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,24 +1,58 @@
 const Item = require("../models/item");
 
 // get all items (public)
-const items_index = (req, res) => {
-  Item.find()
-    .sort({ date: -1 })
-    .then((items) => res.json(items))
-    .catch((err) => {
-      console.log(err);
-    });
+const items_index = async (req, res) => {
+  try {
+    const items = await Item.find().sort({ date: -1 });
+    res.json(items);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "could not find items" });
+  }
 };
 
 // post new item (public)
-const items_index_post = (req, res) => {
-  const newItem = new Item({
-    name: req.body.name,
-  });
-  newItem.save().then((item) => res.json(item));
+const items_index_post = async (req, res) => {
+  try {
+    const newItem = await new Item({
+      name: req.body.name,
+    });
+    newItem.save();
+    res.json(newItem);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: "item failed to post" });
+  }
+};
+
+// delete an item (public)
+const items_details_delete = async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    await item.remove();
+    res.json({ deleted: true });
+  } catch {
+    console.log(err);
+    res.status(404).json({ error: "item failed to delete" });
+  }
+};
+
+// update an item (public)
+const items_details_update = async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    Object.assign(item, req.body);
+    item.save();
+    res.json(item);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ error: "item failed to update" });
+  }
 };
 
 module.exports = {
   items_index,
   items_index_post,
+  items_details_delete,
+  items_details_update,
 };
--- routes/api/itemRoutes.js
+++ routes/api/itemRoutes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const itemController = require("../../controllers/itemController");
 const router = express.Router();
 
-// Item Model
-const Item = require("../../models/item");
-
 router.get("/", itemController.items_index);
 router.post("/", itemController.items_index_post);
+router.delete("/:id", itemController.items_details_delete);
+router.put("/:id", itemController.items_details_update);
 
 module.exports = router;
